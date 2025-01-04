baeafa66e9b9a394c4959b514ec062921c661186
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2025-01-04 10:22:38 +0200

    Added a hero section

---
+++ src/components/containers/Container.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+function Container({ children, className = "" }: Props) {
+  return (
+    <div className={cn("mx-auto max-w-4xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8", className)}>
+      <div className="mx-auto sm:max-w-2xl lg:max-w-none">{children}</div>
+    </div>
+  );
+}
+
+export default Container;
